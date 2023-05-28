9ff3694afa197c5caa5eeb0ff6e0b61d7a905a28
hpetch-smith <hpetchsmith98@gmail.com>
2023-05-28 19:34:09 +0100

    solved problem 1: counting nucleotides

---
+++ count_nuc.py
@@ -0,0 +1,26 @@
+# given a nucleotide sequence return A C G T counts as a string of numbers in same order 
+# count nucleotides
+def count_nucleotides(seq):
+    # nuceleotides to count
+    nucleotides = ['A','C','G','T']
+    # array to store counts 
+    counts = []
+    # loop through nucleotides 
+    for base in nucleotides:
+        # count 
+        counts.append(seq.count(base))
+    # convert to string    
+    counts = map(str,counts)
+    # return in string format
+    return ' '.join(counts)
+
+
+if __name__ == '__main__':
+    # get filename
+    filename = 'rosalind_dna.txt'
+    # open file 
+    with open(filename) as seq:
+        # read content
+        seq = seq.read()
+    
+    print(count_nucleotides(seq))
